506ce5a5e058db173a6135e44e94912e8726c1dd
shonenada <shonenada@gmail.com>
2017-01-06 16:49:17 +0800

    feat(rtm): Impl get members and channels of current team

--- src/rtm/client.rs
+++ src/rtm/client.rs
@@ -1,7 +1,7 @@
 use std::io::Read;
 
-use ws::{connect, Handler, Sender, Handshake};
-use rtm::utils::{http_get, parse_json, get_ws_url};
+use ws::{Sender};
+use rtm::utils::{http_get, parse_json};
 use rtm::data_structure::{Team, User, Channel};
 
 static BASE_URL: &'static str = "https://rtm.bearychat.com/v1";
@@ -17,47 +17,81 @@ impl RTMClient {
         let mut resp = http_get(url.as_str()).unwrap();
         let mut buffer = String::new();
         resp.read_to_string(&mut buffer);
-        let team_resp = parse_json(buffer);
-        match team_resp {
-            Ok(team_resp) => {
-                let code = team_resp["code"].as_i32().unwrap();
+        let team_data= parse_json(buffer);
+        match team_data {
+            Ok(data) => {
+                let code = data["code"].as_i32().unwrap();
                 if code == 0 {
-                    let team_data = team_resp["result"].clone();
+                    let team_data = data["result"].clone();
                     let team = Team::parse_from_json(team_data);
                     return Some(team)
                 } else {
-                    let error = team_resp["error"].clone();
+                    let error = data["error"].clone();
                     panic!("Failed to get team. {}", error);
-                    return None
                 }
             },
             Err(e) => {
                 panic!("Failed to get team. {}", e);
-                return None
             }
         }
     }
 
-    pub fn members(&self) -> Option<User> {
-        let url = format!("{}{}", BASE_URL, "/current_team.members");
+    pub fn members(&self) -> Option<Vec<User>> {
+        let url = format!("{}{}?token={}", BASE_URL, "/current_team.members", self.token);
         let mut resp = http_get(url.as_str()).unwrap();
         let mut buffer = String::new();
         resp.read_to_string(&mut buffer);
-        let members_resp = parse_json(buffer);
-        match members_resp {
-            Ok(members_resp) => {
-                None
+        let members_data = parse_json(buffer);
+        match members_data {
+            Ok(data) => {
+                let code = data["code"].as_i32().unwrap();
+                if code == 0 {
+                    let mut members = Vec::new();
+                    let mut rv = data["result"].clone();
+                    let size = rv.len();
+                    for i in 0..size {
+                        let obj = User::parse_from_json(rv[i].take());
+                        members.push(obj);
+                    }
+                    return Some(members)
+                } else {
+                    let error = data["error"].clone();
+                    panic!("Failed to get members. {}", error);
+                }
             },
             Err(e) => {
-                println!("Failed to get team membres. {}", e);
-                None
+                panic!("Failed to get team membres. {}", e);
             }
         }
     }
 
-    pub fn channels(&self) {
-        let url = format!("{}{}", BASE_URL, "/current_team.channels");
-        let mut resp = http_get(url.as_str());
+    pub fn channels(&self) -> Option<Vec<Channel>> {
+        let url = format!("{}{}?token={}", BASE_URL, "/current_team.channels", self.token);
+        let mut resp = http_get(url.as_str()).unwrap();
+        let mut buffer = String::new();
+        resp.read_to_string(&mut buffer);
+        let channels_data = parse_json(buffer);
+        match channels_data {
+            Ok(data) => {
+                let code = data["code"].as_i32().unwrap();
+                if code == 0 {
+                    let mut channels = Vec::new();
+                    let mut rv = data["result"].clone();
+                    let size = rv.len();
+                    for i in 0..size {
+                        let obj = Channel::parse_from_json(rv[i].take());
+                        channels.push(obj);
+                    }
+                    return Some(channels)
+                } else {
+                    let error = data["error"].clone();
+                    panic!("Failed to get channels. {}", error);
+                }
+            },
+            Err(e) => {
+                panic!("Failed to get team channels. {}", e);
+            }
+        }
     }
 
     fn get_user(&self, uid: i32) {
--- src/rtm/data_structure.rs
+++ src/rtm/data_structure.rs
@@ -3,98 +3,149 @@ use std::fmt::{Display, Result, Formatter};
 use json::{JsonValue};
 use chrono::{UTC, DateTime};
 
+#[allow(dead_code)]
 pub struct Team {
-    id: String,
-    name: String,
-    subdomain: String,
-    email_domain: String,
-    description: String,
-    inactive: bool,
-    logo_url: String,
+    id: Option<String>,
+    name: Option<String>,
+    subdomain: Option<String>,
+    email_domain: Option<String>,
+    description: Option<String>,
+    inactive: Option<bool>,
+    logo_url: Option<String>,
 //    created: DateTime<UTC>,
 //    updated: DateTime<UTC>,
 }
 
 impl Display for Team {
     fn fmt(&self, f : &mut Formatter) -> Result {
-        write!(f, "id: {:?}\nname: {:?}", self.id, self.name)
+        write!(f, "<Team ");
+
+        match self.id.clone() {
+            Some(id) => { write!(f, "id: \"{}\"", id); },
+            None => {}
+        }
+
+        match self.name.clone() {
+            Some(name) => { write!(f, ", name: \"{}\"", name); },
+            None => {}
+        }
+
+        write!(f, ">")
     }
 }
 
 impl Team {
     pub fn parse_from_json(mut data: JsonValue) -> Team {
         Team {
-            id: data["id"].take_string().unwrap(),
-            name: data["name"].take_string().unwrap(),
-            subdomain: data["subdomain"].take_string().unwrap(),
-            email_domain: data["email_domain"].take_string().unwrap(),
-            description: data["description"].take_string().unwrap(),
-            logo_url: data["logo_url"].take_string().unwrap(),
-            inactive: data["inactive"].as_bool().unwrap(),
+            id: data["id"].take_string(),
+            name: data["name"].take_string(),
+            subdomain: data["subdomain"].take_string(),
+            email_domain: data["email_domain"].take_string(),
+            description: data["description"].take_string(),
+            logo_url: data["logo_url"].take_string(),
+            inactive: data["inactive"].as_bool(),
         }
     }
 }
 
+#[allow(dead_code)]
 pub struct User {
-    id: String,
-    name: String,
-    email: String,
-    team_id: String,
-    full_name: String,
-    avatar_url: String,
-    presence: String,
+    id: Option<String>,
+    name: Option<String>,
+    email: Option<String>,
+    team_id: Option<String>,
+    full_name: Option<String>,
+    avatar_url: Option<String>,
+    presence: Option<String>,
+}
+
+impl Display for User {
+    fn fmt(&self, f : &mut Formatter) -> Result {
+        write!(f, "<User ");
+
+        match self.id.clone() {
+            Some(id) => { write!(f, "id: \"{}\"", id); },
+            None => {}
+        }
+
+        match self.name.clone() {
+            Some(name) => { write!(f, ", name: \"{}\"", name); }
+            None => {}
+        }
+
+        write!(f, ">")
+    }
 }
 
 impl User {
     pub fn parse_from_json(mut data: JsonValue) -> User {
         User {
-            id: data["id"].take_string().unwrap(),
-            name: data["name"].take_string().unwrap(),
-            email: data["email"].take_string().unwrap(),
-            team_id: data["team_id"].take_string().unwrap(),
-            full_name: data["full_name"].take_string().unwrap(),
-            avatar_url: data["avatar_url"].take_string().unwrap(),
-            presence: data["presence"].take_string().unwrap(),
+            id: data["id"].take_string(),
+            name: data["name"].take_string(),
+            email: data["email"].take_string(),
+            team_id: data["team_id"].take_string(),
+            full_name: data["full_name"].take_string(),
+            avatar_url: data["avatar_url"].take_string(),
+            presence: data["presence"].take_string(),
         }
     }
 }
 
+#[allow(dead_code)]
 pub struct Channel {
-    id: String,
-    uid: String,
-    name: String,
-    topic: String,
-    team_id: String,
-    vchannel_id: String,
-    description: String,
-    latest_ts: i64,
-    index_symbol: String,
-    is_member: bool,
-    member_count: i32,
-    mention_count: i32,
-    general: bool,
-    inactive: bool,
-    private: bool,
+    id: Option<String>,
+    uid: Option<String>,
+    name: Option<String>,
+    topic: Option<String>,
+    team_id: Option<String>,
+    vchannel_id: Option<String>,
+    description: Option<String>,
+    latest_ts: Option<i64>,
+    index_symbol: Option<String>,
+    is_member: Option<bool>,
+    member_count: Option<i32>,
+    mention_count: Option<i32>,
+    general: Option<bool>,
+    inactive: Option<bool>,
+    private: Option<bool>,
+}
+
+impl Display for Channel {
+    fn fmt(&self, f : &mut Formatter) -> Result {
+        write!(f, "<Channel ");
+
+        match self.id.clone() {
+            Some(id) => { write!(f, "id: \"{}\"", id); }
+            None => {}
+        }
+
+        match self.name.clone() {
+            Some(name) => { write!(f, ", name: \"{}\"", name); }
+            None => {}
+        }
+
+        write!(f, ">")
+    }
 }
 
 impl Channel {
     pub fn parse_from_json(mut data: JsonValue) -> Channel {
         Channel {
-            id: data["id"].take_string().unwrap(),
-            uid: data["uid"].take_string().unwrap(),
-            name: data["name"].take_string().unwrap(),
-            topic: data["topic"].take_string().unwrap(),
-            team_id: data["team_id"].take_string().unwrap(),
-            vchannel_id: data["vchannel_id"].take_string().unwrap(),
-            description: data["description"].take_string().unwrap(),
-            latest_ts: data["latest_ts"].as_i64().unwrap(),
-            index_symbol: data["index_symbol"].take_string().unwrap(),
-            is_member: data["is_member"].as_bool().unwrap(),
-            member_count: data["member_count"].as_i32().unwrap(),
-            mention_count: data["mention_count"].as_i32().unwrap(),
-            general: data["general"].as_bool().unwrap(),
-            inactive: data["inactive"].as_bool().unwrap(),
-            private: data["private"].as_bool().unwrap(),
+            id: data["id"].take_string(),
+            uid: data["uid"].take_string(),
+            name: data["name"].take_string(),
+            topic: data["topic"].take_string(),
+            team_id: data["team_id"].take_string(),
+            vchannel_id: data["vchannel_id"].take_string(),
+            description: data["description"].take_string(),
+            latest_ts: data["latest_ts"].as_i64(),
+            index_symbol: data["index_symbol"].take_string(),
+            is_member: data["is_member"].as_bool(),
+            member_count: data["member_count"].as_i32(),
+            mention_count: data["mention_count"].as_i32(),
+            general: data["general"].as_bool(),
+            inactive: data["inactive"].as_bool(),
+            private: data["private"].as_bool(),
         }
     }
 }
